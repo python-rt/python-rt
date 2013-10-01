08c2070252b6430a148bbf2f16d159f048d3b4f9
Jiri Machalek <jiri.machalek@nic.cz>
2013-10-01 15:35:48 +0200

    bump version to 1.0.7

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 setup(name='rt',
-    version='1.0.post6',
+    version='1.0.7',
     description='Python interface to Request Tracker API',
     long_description=README,
     license='GNU General Public License (GPL)',
     author='Jiri Machalek',
     author_email='jiri.machalek@nic.cz',
     url='https://gitlab.labs.nic.cz/labs/python-rt',
-    install_requires=['requests'],
+    install_requires=['requests', 'six'],
     py_modules=['rt'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
