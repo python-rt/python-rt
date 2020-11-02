18c2e93ea302a4e74174b28e582c0b9a53a3bd45
Gabriel Filion <gabster@lelutin.ca>
2020-11-02 01:59:33 -0500

    migrate REST2 tests to pytest
    The initial implementation of tests was written with the "unittest"
    library since it was what was already used for REST 1.x tests. In issue
    34 sim0nx expressed a preference for using py.test, so let's migrate
    over to this testing library before we have too much tests written.
    
    pytest is able to run the tests previously written for the REST 1.x
    imlementation since they were using unittest.
    
    Since nose is unable to run tests written for pytest, we also need to
    change our CI (and thus dev requirements) to run tests with pytest.
    This must also include a change in coverage report method to pytest-cov,
    which produces a ".coverage" sqlite database that we must now add to
    ignored files.

--- tests/test_functional_rest2_authentication.py
+++ tests/test_functional_rest2_authentication.py
@@ -1,68 +1,64 @@
 """Test function of an actual login against a live RT instance.
 
+This file aims to verify which different authentication methods work and which
+don't. It will also help to track if an authentication method that's broken
+will eventually be fixed in a certain RT version.
+
  * The tests in this file all require a live RT instance that can receive
    requests.
  * The tests also require that the credential information be valid for
    permitting login to the RT instance.
- * You need to export the information about the instance as environment
-   variables:
-   * RT_BASE_URL: URL of your RT instance (without the "REST/2.0/" suffix)
-   * RT_USER, RT_PASS: username and password that are used for normal
-     authentication
-   * RT_TOKEN: a valid token that will let tests contact the API.
 
-This file aims to verify which different authentication methods work and which
-don't. It will also help to track if an authentication method that's broken
-will eventually be fixed in a certain RT version.
+This file uses credentials that are valid for the docker image
+netsandbox/request-tracker:4.4, so you can use this image to run tests
+locally.
 """
-import os
-import unittest
+import pytest
+
 import requests
 from urllib.parse import urljoin, quote
 
 
-class TestFunctionalRTAuthentication(unittest.TestCase):
+@pytest.fixture
+def instance_info(scope="session"):
+    """Information about instance under test."""
+    return {
+        "url": "http://localhost:8080/",
+        "user": "root",
+        "pass": "password",
+        "token": "1-14-85d105ea058f445f6b4cc5811e0079cb",
+    }
+
+
+class TestFunctionalRTAuthentication:
     """Try authenticating to a live instance using different methods.
 
     .. seealso:: https://docs.bestpractical.com/rt/5.0.0/RT/REST2.html#Authentication-Methods
     """  # noqa: E501
 
-    def setUp(self):
-        """Grab RT credentials from the environment."""
-        try:
-            self.RT_BASE_URL = os.environ['RT_BASE_URL']
-            self.INSTANCE_CREDENTIALS = {
-                "user": os.environ['RT_USER'],
-                "pass": os.environ['RT_PASS'],
-            }
-            self.AUTH_TOKEN = os.environ['RT_TOKEN']
-        except KeyError as e:
-            raise Exception(
-                "Environment variable was not defined: {}".format(e))
-
-    def test_http_basic_auth(self):
+    def test_http_basic_auth(self, instance_info):
         """Use HTTP Basic Authentication to make an authenticated request.
 
         Contrary to what the REST2 API documents, the HTTP Basic
         Authentication does not seem to work against the REST2 API on either
         an RT 4.x or a 5.x instance.
         """
-        API_URL = urljoin(self.RT_BASE_URL, "REST/2.0")
+        API_URL = urljoin(instance_info["url"], "REST/2.0")
         basic_auth = (
-            self.INSTANCE_CREDENTIALS["user"],
-            self.INSTANCE_CREDENTIALS["pass"]
+            instance_info["user"],
+            instance_info["pass"]
         )
 
         # If this test ever fails consistently, it might mean that HTTP Basic
         # Auth is suddenly working!
-        with self.assertRaises(requests.exceptions.HTTPError):
+        with pytest.raises(requests.exceptions.HTTPError):
             r = requests.get(API_URL, auth=basic_auth)
             r.raise_for_status()
 
-    def test_token_auth(self):
+    def test_token_auth(self, instance_info):
         """Use a token to run an authenticated request to the API."""
-        endpoint_url = urljoin(self.RT_BASE_URL, "REST/2.0/rt")
-        prepared_token = quote(self.AUTH_TOKEN)
+        endpoint_url = urljoin(instance_info["url"], "REST/2.0/rt")
+        prepared_token = quote(instance_info["token"])
         headers = {
             "Authorization": "token {}".format(prepared_token),
         }
@@ -72,7 +68,7 @@ class TestFunctionalRTAuthentication(unittest.TestCase):
 
         assert len(r.json()) == 2
 
-    def test_http_session_cookie_auth(self):
+    def test_http_session_cookie_auth(self, instance_info):
         """Use the cookie of a session authenticated outside of the API.
 
         According to the API's documentation, this method is not recommended
@@ -80,9 +76,13 @@ class TestFunctionalRTAuthentication(unittest.TestCase):
         currently broken, it is the only method we can use to authenticated
         without a token.
         """
-        auth_url = urljoin(self.RT_BASE_URL, "NoAuth/Login.html")
+        auth_url = urljoin(instance_info["url"], "NoAuth/Login.html")
 
-        r = requests.post(auth_url, data=self.INSTANCE_CREDENTIALS)
+        credentials = {
+            "user": instance_info["user"],
+            "pass": instance_info["pass"],
+        }
+        r = requests.post(auth_url, data=credentials)
         r.raise_for_status()
 
         assert len(dict(r.cookies)) == 1
--- tests/test_rt2.py
+++ tests/test_rt2.py
@@ -1,8 +1,4 @@
 """Tests for the REST2 API client."""
-import unittest
-from unittest.mock import patch
-
-
 from rt import rest2
 
 from requests import Session
@@ -11,13 +7,13 @@ from requests import Session
 REST2_URL = "http://localhost/rt/REST/2.0/"
 
 
-class TestRest2(unittest.TestCase):
+class TestRest2:
     """Group of test cases for the REST2 API client."""
 
-    @patch('rt.rest2.requests.sessions.default_headers')
-    def test_rest2_constructor(self, default_headers):
+    def test_rest2_constructor(self, monkeypatch):
         """All instance attributes are in place in a new instance."""
-        default_headers.return_value = {}
+        monkeypatch.setattr(
+            'rt.rest2.requests.sessions.default_headers', lambda: {})
         api = rest2.Rest2(REST2_URL)
         assert api.url == REST2_URL
         assert isinstance(api.session, Session)
@@ -26,7 +22,3 @@ class TestRest2(unittest.TestCase):
             'Accept': 'application/json',
             'Accept-Charset': 'utf-8'
         }
-
-
-if __name__ == '__main__':
-    unittest.main()
