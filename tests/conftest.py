6852147a374fbdc31b3a6e3265541a0746d29bff
Georges Toth <georges@trypill.org>
2022-04-18 03:31:55 +0200

    import module in fixture to be able to run tests with typeguard

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 import requests.auth
+import typing
 
-import rt.rest2
+if typing.TYPE_CHECKING:
+    import rt.rest2
 
 # blank setup defaults
 RT_URL = 'http://localhost:8080/REST/2.0/'
@@ -10,7 +12,8 @@ RT_PASSWORD = 'password'
 RT_QUEUE = 'General'
 
 
-@pytest.fixture(scope="session")
-def rt_connection() -> rt.rest2.Rt:
+@pytest.fixture(scope='session')
+def rt_connection() -> 'rt.rest2.Rt':
     """Setup a generic connection."""
+    import rt.rest2
     return rt.rest2.Rt(url=RT_URL, http_auth=requests.auth.HTTPBasicAuth(RT_USER, RT_PASSWORD), http_timeout=None)
